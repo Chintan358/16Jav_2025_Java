75fb82348c1c637605b034d3fac1ed62a2b12c64
chintan <chintan.tops@gmail.com>
2025-01-21 14:16:22 +0530

    all

---
+++ Learninig_Corejava/src/pettern/P9.java
@@ -0,0 +1,55 @@
+package pettern;
+
+
+
+
+
+//    *
+//   ***
+//  *****
+//   ***
+//    *
+
+public class P9 {
+
+	public static void main(String[] args) {
+		
+		
+		int lines = 15;
+		int starCount = 1;
+		int spaceCount = lines-1;
+		int mid  =(lines/2)+1;
+		
+		for(int j=1;j<=lines;j++)
+		{
+			for(int k=1;k<=spaceCount;k++)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int i=1;i<=starCount;i++)
+			{
+				if(i==1 || i==starCount || j==mid)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+			if(j<mid)
+			{
+				starCount+=2;
+				spaceCount--;
+			}
+			else
+			{
+				starCount-=2;
+				spaceCount++;
+			}
+		}
+	}
+	
+}
